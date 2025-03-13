8d3358ffc5f2933f722ee27dc1af1317ef2ec669
Diogo Dourado <douradofadiogo@gmail.com>
2025-03-13 15:24:28 -0300

    [feat][WIP] Update the code to run a simple project by terminal or runner with the setup and tear-down isolate

--- pytest_with_core_plugin/pytest_zero/runner.py
+++ pytest_with_core_plugin/pytest_zero/runner.py
@@ -1,16 +1,20 @@
 import sys
+from copy import deepcopy
 
 import pytest
 
 def get_args():
-    args = sys.argv
+    args =deepcopy(sys.argv)
     args.pop(0)
     return args
 
 def main():
+    additional_args = get_args()
     # https://pytest-html.readthedocs.io/en/latest/installing.html
     # https://docs.pytest.org/en/6.2.x/usage.html
     pytest_commands = ['-s','-v','--log-level=info','--tb=auto', '--html', 'report.html', '--self-contained-html']
+    if additional_args:
+        pytest_commands = pytest_commands + additional_args
     pytest.main(pytest_commands)
 
 
---
+++ pytest_with_core_plugin/pytest_zero/tests/__init__.py
@@ -0,0 +1,7 @@
+import logging
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,  # Set the logging level
+    format="%(asctime)s - %(name)s - %(levelname)s - Conftest - %(message)s",  # Log format
+)
